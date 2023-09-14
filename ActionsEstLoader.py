ef60696e1ad93264d3a39caf5b41e0d6a799ad19
passioneffort <agronkercishta.ai@gmail.com>
2023-09-14 12:25:26 -0400

    modify

--- ActionsEstLoader.py
+++ ActionsEstLoader.py
@@ -27,28 +27,28 @@ class TSSTG(object):
         self.model.load_state_dict(torch.load(weight_file))
         self.model.eval()
 
-    def predict(self, pts, image_size):
-        """Predict actions from single person skeleton points and score in time sequence.
-        Args:
-            pts: (numpy array) points and score in shape `(t, v, c)` where
-                t : inputs sequence (time steps).,
-                v : number of graph node (body parts).,
-                c : channel (x, y, score).,
-            image_size: (tuple of int) width, height of image frame.
-        Returns:
-            (numpy array) Probability of each class actions.
-        """
-        pts[:, :, :2] = normalize_points_with_size(pts[:, :, :2], image_size[0], image_size[1])
-        pts[:, :, :2] = scale_pose(pts[:, :, :2])
-        pts = np.concatenate((pts, np.expand_dims((pts[:, 1, :] + pts[:, 2, :]) / 2, 1)), axis=1)  # 加入脖子中心点坐标 @yjy
+    # def predict(self, pts, image_size):
+    #     """Predict actions from single person skeleton points and score in time sequence.
+    #     Args:
+    #         pts: (numpy array) points and score in shape `(t, v, c)` where
+    #             t : inputs sequence (time steps).,
+    #             v : number of graph node (body parts).,
+    #             c : channel (x, y, score).,
+    #         image_size: (tuple of int) width, height of image frame.
+    #     Returns:
+    #         (numpy array) Probability of each class actions.
+    #     """
+    #     pts[:, :, :2] = normalize_points_with_size(pts[:, :, :2], image_size[0], image_size[1])
+    #     pts[:, :, :2] = scale_pose(pts[:, :, :2])
+    #     pts = np.concatenate((pts, np.expand_dims((pts[:, 1, :] + pts[:, 2, :]) / 2, 1)), axis=1)  # 加入脖子中心点坐标 @yjy
 
-        pts = torch.tensor(pts, dtype=torch.float32)
-        pts = pts.permute(2, 0, 1)[None, :]
+    #     pts = torch.tensor(pts, dtype=torch.float32)
+    #     pts = pts.permute(2, 0, 1)[None, :]
 
-        mot = pts[:, :2, 1:, :] - pts[:, :2, :-1, :]
-        mot = mot.to(self.device)
-        pts = pts.to(self.device)
+    #     mot = pts[:, :2, 1:, :] - pts[:, :2, :-1, :]
+    #     mot = mot.to(self.device)
+    #     pts = pts.to(self.device)
 
-        out = self.model((pts, mot))
+    #     out = self.model((pts, mot))
 
-        return out.detach().cpu().numpy()
+    #     return out.detach().cpu().numpy()
